c0676ef90dea772596d0339284301743c2038cda
Lucas Gonzaga <130797799+arctouch-lucasgonzaga@users.noreply.github.com>
2023-09-01 16:48:13 -0300

    resolve bug

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,22 @@ export function loadComponent() {
   document.querySelector(`#${componentId}`).innerHTML = headerHTML;
 }
 
+export function dropdownProducts() {
+  function myFunction() {
+    document.getElementById("productsDropContent").classList.toggle("show");
+  }
+  myFunction()
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    const myDropdown = document.getElementById("productsDropContent");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 export default {
   componentId,
   loadComponent,
+  dropdownProducts,
 };
