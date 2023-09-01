9c91b5b97300abc79ba8569602ba585d4ba8489e
Bruno Cordioli <bruno.cordioli@arctouch.com>
2023-09-01 17:17:32 -0300

    feat: new component inside header called dropdown-product

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,29 @@
 import "./header.scss";
 import headerHTML from "./header.html?raw";
-
+import dropdownProduct from "./dropdown-product/dropdown-product";
 export const componentId = "header";
 
 export function loadComponent() {
   document.querySelector(`#${componentId}`).innerHTML = headerHTML;
+  dropdownProduct.loadComponent();
 }
 
-export function dropdownProducts() {
-  function myFunction() {
-    document.getElementById("productsDropContent").classList.toggle("show");
-  }
-  myFunction()
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    const myDropdown = document.getElementById("productsDropContent");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// export function dropdownProducts() {
+//   function myFunction() {
+//     document.getElementById("productsDropContent").classList.toggle("show");
+//   }
+//   myFunction();
+//   window.onclick = function (e) {
+//     if (!e.target.matches(".dropbtn")) {
+//       const myDropdown = document.getElementById("productsDropContent");
+//       if (myDropdown.classList.contains("show")) {
+//         myDropdown.classList.remove("show");
+//       }
+//     }
+//   };
+// }
 export default {
   componentId,
   loadComponent,
-  dropdownProducts,
+  // dropdownProducts,
 };
